df4c8ab79c0a81138fc251082ffefe8a38433a48
Abdullah Javed <abdullahjaved65@yahoo.com>
2023-01-20 15:54:20 +0500

    Registration Functionality Completed. Started Comment Functionality in room.html

--- base/views.py
+++ base/views.py
@@ -9,6 +9,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 from .forms import RoomForm
 from .models import Room, Topic
@@ -20,7 +21,7 @@ def loginPage(request):
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -49,7 +50,20 @@ def logoutUser(request):
 
 def registerPage(request):
     page = 'register'
-    return render(request, 'base/login_register.html')
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit="False")
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "An Error Occurred "
+                                    "During The Registration")
+    return render(request, 'base/login_register.html',
+                  {'form': form})
 
 
 def home(request):
