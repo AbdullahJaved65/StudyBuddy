c6ebe1e7e69f838989a8fc9e4ff47f24d442af58
Abdullah Javed <abdullahjaved65@yahoo.com>
2023-02-06 15:10:59 +0500

    - Day 3 of theme installation - Completed Theme installation, but still some rough edges in CSS - Changed back-end in views.py for creation and updating of forms

--- base/views.py
+++ base/views.py
@@ -120,15 +120,21 @@ def userProfile(request, pk):
 @login_required(login_url='login')
 def createRoom(request):
     form = RoomForm()
+    topics = Topic.objects.all()
 
     if request.method == 'POST':
-        form = RoomForm(request.POST)
-        if form.is_valid():
-            room = form.save(commit=False)
-            room.host = request.user
-            room.save()
-            return redirect('home')
-    context: dict[Any, Any] = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+
+        Room.objects.create(
+            host=request.user,
+            topic=topic,
+            name=request.POST.get('name'),
+            description=request.POST.get('description')
+        )
+        return redirect('home')
+
+    context: dict[Any, Any] = {'form': form, 'topics': topics}
     return render(request, 'base/room_form.html', context)
 
 
@@ -136,17 +142,20 @@ def createRoom(request):
 def updateRoom(request, pk):
     update_room = Room.objects.get(id=pk)
     form = RoomForm(instance=update_room)
-
+    topics = Topic.objects.all()
     if request.user != update_room.host:
         return HttpResponse("You are not allowed here! "
                             "Please Return Back")
 
     if request.method == 'POST':
-        form = RoomForm(request.POST, instance=update_room)
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    context = {'form': form}
+        topic_name = request.POST.get('topic')
+        topic, created = Topic.objects.get_or_create(name=topic_name)
+        update_room.name = request.POST.get('name')
+        update_room.topic = topic
+        update_room.description = request.POST.get('description')
+        update_room.save()
+        return redirect('home')
+    context = {'form': form, 'topics': topics, 'room': update_room}
     return render(request, 'base/room_form.html', context)
 
 
