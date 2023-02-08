ef6df26bdd940b42831ca3140427d695acf0a0fe
Abdullah Javed <abdullahjaved65@yahoo.com>
2023-02-08 13:15:27 +0500

    - Added Mobile Responsiveness (still a little rough) - Added Mobile Activities and Topics Page with Search Functionalities.

--- base/views.py
+++ base/views.py
@@ -78,7 +78,7 @@ def home(request):
         Q(room__topic__name__icontains=q)
     )
     room_count = rooms.count()
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:3]
     context = {'rooms': rooms,
                'topics': topics,
                'room_count': room_count,
@@ -200,3 +200,17 @@ def updateUser(request):
 
     context = {'form': form}
     return render(request, 'base/update-user.html', context)
+
+
+def topicsPage(request):
+    q = request.GET.get('q') \
+        if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    context = {"topics": topics}
+    return render(request, 'base/topics.html', context)
+
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages': room_messages}
+    return render(request, 'base/activity.html', context)
